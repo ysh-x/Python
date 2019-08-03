a7eb1690f35e45e277d24d9e65fe31204113c600
Yogeshwar G <42147996+Gygshwr@users.noreply.github.com>
2019-08-03 08:45:52 +0530

    This program performs all the arithmetic operation

---
+++ Arith.py
@@ -0,0 +1,22 @@
+numberone = 10
+numbertwo = 2
+add = numberone+numbertwo
+diff = numberone-numbertwo
+mul = numberone*numbertwo
+div = numberone/numbertwo
+floor = numberone//numbertwo
+module = numberone%numbertwo
+expb = numbertwo**numberone
+expa = numberone**numbertwo
+
+
+print('Addition',add)
+print('Subraction',diff)
+print('Multiplication',mul)
+print('division',div)
+print('Floor',floor)
+print('Module',module)
+print('A raise to B ',expa)
+print('B raise to A',expb)
+
+
