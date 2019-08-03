c359eea248f034ac2069c4e84f8e94c57ac1d72d
Yogeshwar G <42147996+Gygshwr@users.noreply.github.com>
2019-08-03 09:01:03 +0530

    What's the time now ?

---
+++ clock.py
@@ -0,0 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+
+print('+------------CLOCK-------------+')
+print("|--TIME--",now.hour,":",now.minute,":",now.second,"------- |")
+print('+--DATE--',now.year,":",now.month,":",now.day,"--------+")
