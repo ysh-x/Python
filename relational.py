5d41fe16dd0b14c27cff775432d6b0c5dd1c1449
Yogeshwar G <42147996+Gygshwr@users.noreply.github.com>
2019-08-03 08:53:33 +0530

    Let's learn relational operators

---
+++ relational.py
@@ -0,0 +1,14 @@
+numberone = 4
+numbertwo = 5
+
+print('Number One - ',numberone)
+print('Number two - ',numbertwo)
+print()
+print(' Is number one equal to number two ? ',numberone==numbertwo)
+print(' Is number one grater to number two ? ',numberone>numbertwo)
+print(' Is number one lesser to number two ? ',numberone<numbertwo)
+print(' Is number one not equal to number two ? ',numberone!=numbertwo)
+print(' Is number one grater than or equal to number two ? ',numberone>=numbertwo)
+print(' Is number one lesser than or equal to number two ? ',numberone<=numbertwo)
+
+
