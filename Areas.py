1245de8bbfab4c4ca46765f5686ba8381596eb1f
Yogeshwar G <42147996+ysh-x@users.noreply.github.com>
2020-01-01 18:43:33 +0530

    Calculate the area of a circle

---
+++ Areas.py
@@ -0,0 +1,11 @@
+#Integer - Lesson One 
+#Using the built in functions of float and Integer
+
+PI = 22/7 #Use of a constant
+radius = float(input("RADIUS - "))
+area = abs(PI*radius*radius) #Area cannot be negative
+print(f"THE AREA OF THE CIRCLE OF RADIUS {radius} is {area}\n") #Formatted strings
+print(f"THE ROUNDED OFF AREA OF THE CIRCLE OF RADIUS {radius} is {round(area)}\n") #Formatted strings and Escape sequence
+ 
+
+
